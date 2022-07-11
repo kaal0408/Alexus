350f9a9f70c19034460926cab6d6d259c2bacd3e
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-11 17:51:15 +0530

    Create __main__.py

---
+++ main/__main__.py
@@ -0,0 +1,27 @@
+
+
+from .client import bot, asst
+from pyrogram.errors import AccessTokenInvalid, ApiIdInvalid, ApiIdPublishedFlood
+from pytgcalls import PyTgCalls
+from pytgcalls import idle as pyidle
+from pytgcalls import PyTgCalls
+
+
+
+if __name__ == "__main__":
+    try:
+        asst.start()  # Not using run as wanna print 
+        print("•×•Yeahhhh boi Assistant bot Started•×•")
+        bot.run() # using run for session client
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your TOKEN is not valid.")
+
+bot = bot
+call_py = PyTgCalls(bot)
+
+call_py.start()
+print("Vc Client Started")
+pyidle()
+idle()
