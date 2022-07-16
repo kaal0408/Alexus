5aa57353e803ca0bfb2d4d028910baf02a629318
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-16 18:12:28 +0530

    Ping Module

--- modules/ping.py
+++ modules/ping.py
@@ -12,4 +12,4 @@ async def ping(_, msg: Message):
     start_time = datetime.now()
     end_time = datetime.now()
     uptime = ((end_time - start_time).microseconds / 1000)
-    await asst.send_message(msg.chat.id, "❖𝗣 𝗢 𝗡 𝗚❖ ♪\nᴍs: {uptime}s!!")
+    await asst.send_message(msg.chat.id, f"❖𝗣 𝗢 𝗡 𝗚❖ ♪\nᴍs: {uptime}s!!")
