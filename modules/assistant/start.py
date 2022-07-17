22d47c31833472d47d36f9946467ae671e712732
TeamUzox <106080156+TeamUzox@users.noreply.github.com>
2022-07-17 19:54:00 +0530

    Update start.py

--- modules/assistant/start.py
+++ modules/assistant/start.py
@@ -6,18 +6,22 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @asst.on_message(filters.command("start") & filters.private)
 async def sstart(_,message : Message):
-    await message.reply_text(
-    f""" **ᴀ ᴡᴀʀᴍ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀʟᴇxᴜs !!
-    ɪ'ᴍ ʏᴏᴜʀ ᴀᴅᴠᴀɴᴄᴇ ʙᴏᴛ ɴᴏ ʟᴀɢ ɢʀᴏᴜᴘ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ
+    await message.reply_photo(
+    photo=f"https://te.legra.ph/file/900d41c5f02171fd21c9d.jpg",
+    caption=f"""**ᴀ ᴡᴀʀᴍ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀʟᴇxᴜs !! ɪ'ᴍ ʏᴏᴜʀ ᴀᴅᴠᴀɴᴄᴇ ʙᴏᴛ ɴᴏ ʟᴀɢ ɢʀᴏᴜᴘ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ
 
     ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ➪ [ᴅᴇᴠ](https://t.me/Alone_loverBoy) ᴀɴᴅ [ᴅᴇᴠ](https://t.me/nobitadev)
 
-   ☺️ ᴋᴇᴇᴘ sᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ғᴀᴄᴇ Aʟᴡᴀʏs ☺️** """,
+   ☺️ ᴋᴇᴇᴘ sᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ғᴀᴄᴇ Aʟᴡᴀʏs ☺️ **""",
 reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
                         "♢🇦 🇩 🇩   🇲 🇪♢", url=f"https://t.me/alexusMusic_Bot?startgroup=true")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "♢🇸 🇺 🇵 🇵 🇴 🇷 🇹♢", url=f"https://t.me/TeamAlexus")
                 ]
 
             ]
