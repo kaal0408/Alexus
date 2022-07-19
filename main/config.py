e368ce0b479b3d2d7a34b92470a3c0b2389eae26
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-19 15:03:46 +0530

    Update config.py

--- main/config.py
+++ main/config.py
@@ -1,8 +1,9 @@
 import os 
 
 #----Sudo Configuration----#
+que = {}
 SUDO_ID = os.environ.get("SUDO_ID", "")
 DURATION_LIMIT = os.environ.get("DURATION_LIMIT", 100)
 COMMAND_PREFIXES = (os.environ.get("COMMAND_PREFIXES", "/ !").split()
-que = {}
+
 
