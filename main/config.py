0513e88349941914c21d81d9719bd6934f2bcc69
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-19 14:25:05 +0530

    Helpers Functions

--- main/config.py
+++ main/config.py
@@ -3,3 +3,6 @@ import os
 #----Sudo Configuration----#
 SUDO_ID = os.environ.get("SUDO_ID", "")
 DURATION_LIMIT = os.environ.get("DURATION_LIMIT", 100)
+COMMAND_PREFIXES = (os.environ.get("COMMAND_PREFIXES", "/ !").split()
+
+
