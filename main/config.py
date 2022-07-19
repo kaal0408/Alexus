0fff8841d399ce3468556cdc0340450b72275033
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-19 14:59:29 +0530

    i d k

--- main/config.py
+++ main/config.py
@@ -4,5 +4,5 @@ import os
 SUDO_ID = os.environ.get("SUDO_ID", "")
 DURATION_LIMIT = os.environ.get("DURATION_LIMIT", 100)
 COMMAND_PREFIXES = (os.environ.get("COMMAND_PREFIXES", "/ !").split()
-
+que = {}
 
