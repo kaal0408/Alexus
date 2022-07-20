ebbdaf0722b678455c58a039767ef1ddf9e8072e
TeamUzox <106080156+TeamUzox@users.noreply.github.com>
2022-07-20 20:49:23 +0530

    Update start.py

--- modules/assistant/start.py
+++ modules/assistant/start.py
@@ -8,11 +8,13 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def sstart(_,message : Message):
     await message.reply_photo(
     photo=f"https://te.legra.ph/file/900d41c5f02171fd21c9d.jpg",
-    caption=f"""**ᴀ ᴡᴀʀᴍ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀʟᴇxᴜs !! ɪ'ᴍ ʏᴏᴜʀ ᴀᴅᴠᴀɴᴄᴇ ɢʀᴏᴜᴘ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ\n\nI am very fast and relaible bot with advanced features!!!\nCreated by python3 and Pyrogram!\n\n
+    caption=f"""**ᴀ ᴡᴀʀᴍ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀʟᴇxᴜs !!
+ɪ'ᴍ ʏᴏᴜʀ ᴀᴅᴠᴀɴᴄᴇ ɢʀᴏᴜᴘ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ\n\nɪ ᴀᴍ ᴠᴇʀʏ ғᴀsᴛ ᴀɴᴅ ʀᴇʟᴀɪʙʟᴇ ʙᴏᴛ ᴡɪᴛʜ ᴀᴅᴠᴀɴᴄᴇᴅ ғᴇᴀᴛᴜʀᴇs!!!
+ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴘʏᴛʜᴏɴ3 ᴀɴᴅ ᴘʏʀᴏɢʀᴀᴍ!\n\n
 
-    ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ➪ [Alone_loverboy](https://t.me/Alone_loverBoy)\n\n[Nobita](https://t.me/nobitadev)
+ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ➪ [ᴀʟᴏɴᴇ_ʟᴏᴠᴇʀʙᴏʏ](https://t.me/Alone_loverBoy)\n\n[ɴᴏʙɪᴛᴀ](https://t.me/nobitadev)
 
-   ☺️ ᴋᴇᴇᴘ sᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ғᴀᴄᴇ Aʟᴡᴀʏs ☺️ **""",
+          ☺️ ᴋᴇᴇᴘ sᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ғᴀᴄᴇ Aʟᴡᴀʏs ☺️ **""",
 reply_markup=InlineKeyboardMarkup(
             [
                 [
