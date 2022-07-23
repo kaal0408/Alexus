bd50c08952d579173b737a9ae113dd1515ffc638
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-23 23:36:32 +0530

    Update converter.py

--- services/converter/converter.py
+++ services/converter/converter.py
@@ -1,41 +1,38 @@
 import asyncio
+
 from os import path
 
-class FFmpegReturnCodeError(Exception):
-    pass
-  
-async def convert(file_path):
-    out = path.basename(file_path)
-    out = out.split(".")
-    out[-1] = "raw"
-    out = ".".join(out)
-    out = path.basename(out)
-    out = path.join("raw_files", out)
+from Musical.helpers.errors import FFmpegReturnCodeError
+
+async def convert(file_path: str) -> str:
+
+    out = path.join('raw_files', path.basename(file_path + '.raw'))
 
     if path.isfile(out):
-        return out
-
-    try:
-        proc = await asyncio.create_subprocess_shell(
-            cmd=(
-                "ffmpeg " 
-                "-y -i " 
-                f"{file_path} "
-                "-f s16le "
-                "-ac 1 "
-                "-ar 48000 "
-                "-acodec pcm_s16le " 
-                f"{out}"
-            ),
-            stdin=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-
-        await proc.communicate()
-
-        if proc.returncode != 0:
-            raise FFmpegReturnCodeError("FFmpeg did not return 0")
 
         return out
-    except:
-        raise FFmpegReturnCodeError("FFmpeg did not return 0")
+
+    proc = await asyncio.create_subprocess_shell(
+
+        cmd=(
+
+            'ffmpeg '
+            '-y -i '
+            f'{file_path} '
+            '-f s16le '
+            '-ac 2 '
+            '-ar 48000 '
+            '-acodec pcm_s16le '
+            f'{out}'
+        ),
+
+        stdin=asyncio.subprocess.PIPE,
+
+        stderr=asyncio.subprocess.PIPE,
+
+    )
+
+    await proc.communicate()
+    if proc.returncode != 0:
+        raise FFmpegReturnCodeError('FFmpeg did not return 0')
+    return out
