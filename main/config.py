b8b828ae5fa369067c6075fab4bf80f0bd04d66e
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-25 21:33:26 +0530

    Duration Limit

--- main/config.py
+++ main/config.py
@@ -5,7 +5,7 @@ from os import getenv
 # que = {}
 SUDO_ID = list(map(int, getenv("SUDO_ID").split()))
 #----Configuration----#
-DURATION_LIMIT = os.environ.get("DURATION_LIMIT", 100)
+DURATION_LIMIT = os.environ.get("DURATION_LIMIT", 200)
 COMMAND_PREFIXES = (os.environ.get("COMMAND_PREFIXES", "/ !").split())
 ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
 BOT_NAME = os.environ.get("BOT_NAME", "")
