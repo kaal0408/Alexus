bf2c8ad967e838bb96c0d8f114d953693fdbaabb
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-07-25 08:14:53 +0530

    recalling method changed.

--- main/__main__.py
+++ main/__main__.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     try:
         asst.start()  # Not using run as wanna print 
         print("•×•Yeahhhh boi Assistant bot Started•×•")
-        bot.run() # using run for session client
+       # bot.run() # using run for session client
     except (ApiIdInvalid, ApiIdPublishedFlood):
         raise Exception("Your API_ID/API_HASH is not valid.")
     except AccessTokenInvalid:
